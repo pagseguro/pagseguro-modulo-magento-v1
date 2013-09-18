61ae48010a38b4c0e597ab8ff55d6434b01cf35a
brunolanaros2it <bruno.lanaro@s2it.com.br>
2013-09-18 12:08:03 -0300

    ATUALIZAÇÃO TRAVIS
    Corrigido erros apontados pelo Travis

--- code/local/Mage/Adminhtml/Block/Sales/Order/Grid.php
+++ code/local/Mage/Adminhtml/Block/Sales/Order/Grid.php
@@ -77,61 +77,78 @@ class Mage_Adminhtml_Block_Sales_Order_Grid extends Mage_Adminhtml_Block_Widget_
         );
 
         if (!Mage::app()->isSingleStoreMode()) {
-            $this->addColumn('store_id',
+            $this->addColumn(
+                'store_id',
                 array(
                     'header'    => Mage::helper('sales')->__('Purchased From (Store)'),
                     'index'     => 'store_id',
                     'type'      => 'store',
                     'store_view'=> true,
-                    'display_deleted' => true));
+                    'display_deleted' => true)
+            );
         }
         
-        $this->addColumn('transaction_code',
+        $this->addColumn(
+            'transaction_code',
             array(
                 'header' => Mage::helper('sales')->__('Transaction ID'),
-                'index' => 'transaction_code'));
+                'index' => 'transaction_code')
+        );
 
-        $this->addColumn('created_at',
+        $this->addColumn(
+            'created_at',
             array(
                 'header' => Mage::helper('sales')->__('Purchased On'),
                 'index' => 'created_at',
                 'type' => 'datetime',
-                'width' => '100px'));
+                'width' => '100px')
+        );
 
-        $this->addColumn('billing_name',
+        $this->addColumn(
+            'billing_name',
             array(
                 'header' => Mage::helper('sales')->__('Bill to Name'),
-                'index' => 'billing_name'));
+                'index' => 'billing_name')
+        );
 
-        $this->addColumn('shipping_name',
+        $this->addColumn(
+            'shipping_name',
             array(
                 'header' => Mage::helper('sales')->__('Ship to Name'),
-                'index' => 'shipping_name'));
+                'index' => 'shipping_name')
+        );
 
-        $this->addColumn('base_grand_total',
+        $this->addColumn(
+            'base_grand_total',
             array(
                 'header' => Mage::helper('sales')->__('G.T. (Base)'),
                 'index' => 'base_grand_total',
                 'type'  => 'currency',
-                'currency' => 'base_currency_code'));
+                'currency' => 'base_currency_code')
+        );
 
-        $this->addColumn('grand_total',
+        $this->addColumn(
+            'grand_total',
             array(
                 'header' => Mage::helper('sales')->__('G.T. (Purchased)'),
                 'index' => 'grand_total',
                 'type'  => 'currency',
-                'currency' => 'order_currency_code'));
+                'currency' => 'order_currency_code')
+        );
 
-        $this->addColumn('status',
+        $this->addColumn(
+            'status',
             array(
                 'header' => Mage::helper('sales')->__('Status'),
                 'index' => 'status',
                 'type'  => 'options',
                 'width' => '70px',
-                'options' => Mage::getSingleton('sales/order_config')->getStatuses()));
+                'options' => Mage::getSingleton('sales/order_config')->getStatuses())
+        );
 
         if (Mage::getSingleton('admin/session')->isAllowed('sales/order/actions/view')) {
-            $this->addColumn('action',
+            $this->addColumn(
+                'action',
                 array(
                     'header'    => Mage::helper('sales')->__('Action'),
                     'width'     => '50px',
@@ -141,8 +158,7 @@ class Mage_Adminhtml_Block_Sales_Order_Grid extends Mage_Adminhtml_Block_Widget_
                         array(
                             'caption' => Mage::helper('sales')->__('View'),
                             'url'     => array('base'=>'*/sales_order/view'),
-                            'field'   => 'order_id'
-                        )
+                            'field'   => 'order_id')
                     ),
                     'filter'    => false,
                     'sortable'  => false,
