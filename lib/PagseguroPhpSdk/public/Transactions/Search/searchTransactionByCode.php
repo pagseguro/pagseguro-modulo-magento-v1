21ad9844d787eab8152a54a5dc87852bcce1fa91
gabriellucius <gabriel_lucius@outlook.com>
2017-10-29 17:55:33 -0200

    feature: update pagseguro php library to version 3.3.0

--- lib/PagseguroPhpSdk/public/Transactions/Search/searchTransactionByCode.php
+++ lib/PagseguroPhpSdk/public/Transactions/Search/searchTransactionByCode.php
@@ -4,7 +4,7 @@ require_once "../../../vendor/autoload.php";
 
 \PagSeguro\Library::initialize();
 
-$code = '0B64FD7B4F9641378E9C9462982A8B95';
+$code = '8F17A22C51C641C1B6A40AF8C2FF94E2';
 
 try {
     $response = \PagSeguro\Services\Transactions\Search\Code::search(
