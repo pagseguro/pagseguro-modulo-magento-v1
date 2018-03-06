bcddf60796372ef31d91f03cc18ecbb89bef71cc
AndréCristiani <andre.cristiani@s2it.com.br>
2018-03-06 09:51:43 -0300

    replace no front e validações para retornos false

--- app/code/community/UOL/PagSeguro/Helper/Transaction.php
+++ app/code/community/UOL/PagSeguro/Helper/Transaction.php
@@ -173,12 +173,12 @@ class UOL_PagSeguro_Helper_Transaction extends HelperData
             'lastEventDate'     => $this->getOrderMagetoDateConvert($this->pagSeguroTransaction->getLastEventDate()),
             'installmentCount'  => $this->pagSeguroTransaction->getInstallmentCount(),
             'cancelationSource' => $this->getTitleCancellationSourceTransaction($this->pagSeguroTransaction->getCancelationSource()),
-            'discountAmount'    => str_replace(".", ",", $this->pagSeguroTransaction->getDiscountAmount()),
+            'discountAmount'    => $this->pagSeguroTransaction->getDiscountAmount(),
             'escrowEndDate'     => $this->getOrderMagetoDateConvert($this->pagSeguroTransaction->getEscrowEndDate()),
-            'extraAmount'       => str_replace(".", ",", $this->pagSeguroTransaction->getExtraAmount()),
+            'extraAmount'       => $this->pagSeguroTransaction->getExtraAmount(),
             'feeAmount'         => $this->pagSeguroTransaction->getFeeAmount(),
-            'grossAmount'       => str_replace(".", ",", $this->pagSeguroTransaction->getGrossAmount()),
-            'netAmount'         => str_replace(".", ",", $this->pagSeguroTransaction->getNetAmount()),
+            'grossAmount'       => $this->pagSeguroTransaction->getGrossAmount(),
+            'netAmount'         => $this->pagSeguroTransaction->getNetAmount(),
             'creditorFees'      => $this->prepareCreditorFees(),
             'itemCount'         => $this->pagSeguroTransaction->getItemCount(),
             'items'             => $this->prepareItems(),
@@ -196,11 +196,11 @@ class UOL_PagSeguro_Helper_Transaction extends HelperData
         if(!empty($this->pagSeguroTransaction->getCreditorFees()))
         {
             $creditorFees = array(
-                'intermediationRateAmount'  => str_replace(".", ",", $this->pagSeguroTransaction->getCreditorFees()->getIntermediationRateAmount()),
-                'intermediationFeeAmount'   => str_replace(".", ",", $this->pagSeguroTransaction->getCreditorFees()->getIntermediationFeeAmount()),
-                'installmentFeeAmount'      => str_replace(".", ",", $this->pagSeguroTransaction->getCreditorFees()->getInstallmentFeeAmount()),
-                'operationalFeeAmount'      => str_replace(".", ",", $this->pagSeguroTransaction->getCreditorFees()->getOperationalFeeAmount()),
-                'commissionFeeAmount'       => str_replace(".", ",", $this->pagSeguroTransaction->getCreditorFees()->getCommissionFeeAmount())
+                'intermediationRateAmount'  => $this->pagSeguroTransaction->getCreditorFees()->getIntermediationRateAmount(),
+                'intermediationFeeAmount'   => $this->pagSeguroTransaction->getCreditorFees()->getIntermediationFeeAmount(),
+                'installmentFeeAmount'      => $this->pagSeguroTransaction->getCreditorFees()->getInstallmentFeeAmount(),
+                'operationalFeeAmount'      => $this->pagSeguroTransaction->getCreditorFees()->getOperationalFeeAmount(),
+                'commissionFeeAmount'       => $this->pagSeguroTransaction->getCreditorFees()->getCommissionFeeAmount()
             );
         }
         return $creditorFees;
@@ -217,7 +217,7 @@ class UOL_PagSeguro_Helper_Transaction extends HelperData
                     'id'            => $item->getId(),
                     'description'   => $item->getDescription(),
                     'quantity'      => $item->getQuantity(),
-                    'amount'        => str_replace(".", ",", $item->getAmount()),
+                    'amount'        => $item->getAmount(),
                     'weight'        => $item->getWeight(),
                     'shippingCost'  => $item->getShippingCost()
                 );
