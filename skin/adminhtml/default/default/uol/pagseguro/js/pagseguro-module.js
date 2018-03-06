27bbd3d4af7148804a8795d146465bfbc31953a5
AndréCristiani <andre.cristiani@s2it.com.br>
2018-03-06 10:31:57 -0300

    remoção console log

--- skin/adminhtml/default/default/uol/pagseguro/js/pagseguro-module.js
+++ skin/adminhtml/default/default/uol/pagseguro/js/pagseguro-module.js
@@ -538,7 +538,6 @@ function formatReal( int )
 {
     var tmp = int+'';
     tmp = tmp.replace(".", "");
-    console.log(tmp)
     tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
     if( tmp.length > 6 )
             tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
